8aa6d39064ba33f43ac41752c62818e89bc3f432
Hardonian <118695431+Hardonian@users.noreply.github.com>
2026-07-25 03:30:35 +0000

    🧪 Extract URL parsing to function and add edge case tests

---
+++ reply.py
@@ -0,0 +1,10 @@
+import json
+
+reply = [
+    {
+        "comment_id": "5076682487",
+        "reply": "Understood. Acknowledging that this work is now obsolete and stopping work on this task."
+    }
+]
+
+print(json.dumps(reply))
