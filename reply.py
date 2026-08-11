512725e8e731f2bd4a6d423ecdd8acad4d50522f
Hardonian <118695431+Hardonian@users.noreply.github.com>
2026-08-11 01:47:47 +0000

    Add unit tests for get_local_path in profile link audit

---
+++ reply.py
@@ -0,0 +1,10 @@
+import json
+
+replies = [
+    {
+        "comment_id": "5248082233",
+        "reply": "Understood. Acknowledging that this work is superseded by PR #112 and stopping work on this task."
+    }
+]
+
+print(json.dumps(replies))
---
+++ run_reply.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+reply_to_pr_comments --replies '[{"comment_id": "5248082233", "reply": "Understood. Acknowledging that this work is superseded by PR #112 and stopping work on this task."}]'
